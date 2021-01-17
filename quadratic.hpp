b27c69c7ae2c77199e28b905133a5098d5aa5587
ForbiddenKJ <jamesbutcher167@gmail.com>
2021-01-17 11:42:36 +0000

    added Quadratic Nth Term

--- quadratic.hpp
+++ quadratic.hpp
@@ -1,23 +1,64 @@
 #pragma once
 #include <cmath>
 
-double * quad(double a, double b, double c){
-  double d = pow(b,2)-4*a*c;
+class solver{
+public:
+  double * quadraticRoot(double a, double b, double c){
+    double d = pow(b,2)-4*a*c;
 
-  static double result[2];
+    static double result[2];
 
-  if (d < 0){ // If d is less that 0 the results would be an imaginary number
-    throw 20;
+    // If d is less that 0 the results would be an imaginary number
+
+    if (d < 0){
+      throw 20;
+    }
+
+    // Equation has one root
+
+    else if (d == 0){
+      result[0] = (-b+sqrt(d))/(2*a);
+    }
+
+    // Equation has two roots
+
+    else {
+      result[0] = (-b+sqrt(d))/(2*a);
+      result[1] = (-b-sqrt(d))/(2*a);
+    }
+
+    return result;
   }
 
-  else if (d == 0){ // Equation has one root
-    result[0] = (-b+sqrt(d))/(2*a);
+  double * quadraticNthTerm(double *sequence){
+
+    // Find Quadratic
+
+    double firstDiff1 = sequence[1] - sequence[0];
+    double firstDiff2 = sequence[2] - sequence[1];
+    double secondDiff = firstDiff2 - firstDiff1;
+    double a = secondDiff/2;
+    double aSequence[4];
+    double linearSequence[4];
+
+    for (int i = 0; i < 5; i++){
+      aSequence[i] = (a * pow(i,2));
+    }
+
+    for (int i = 0; i < 5; i++){
+      linearSequence[i] = sequence[i] - aSequence[i];
+    }
+
+    // Find Linear
+
+    double b = linearSequence[1] - linearSequence[0];
+    double c = linearSequence[0] - b;
+
+    // Return Results
+
+    static double result[3] = {a, b, c};
+
+    return result;
   }
 
-  else { // Equation has two roots
-    result[0] = (-b+sqrt(d))/(2*a);
-    result[1] = (-b-sqrt(d))/(2*a);
-  }
-
-  return result;
-}
+};
