7f8dbf769e88b6717ab6a49cb238bf5a59d3b74a
ForbiddenKJ <jamesbutcher167@gmail.com>
2021-01-17 11:44:15 +0000

    added Quadratic Nth Term example

--- main.cpp
+++ main.cpp
@@ -2,9 +2,24 @@
 #include "quadratic.hpp"
 
 int main(){
+
+  /*
   double *p;
-  p = quad(1,3,2);
+  solver Solver;
+  p = Solver.quadraticRoot(1,6,9);
   std::cout << *(p+0) << "," << *(p+1);
+  */
+
+
+  double* p;
+  double data[4] = {9, 13, 19, 27};
+  solver Solution;
+  p = Solution.quadraticNthTerm(data);
+  //std::cout << *(p+0) << "," << *(p+1);
+  //std::cout << *(p+0);
+  for (int i = 0; i < 3; i++){
+    std::cout << *(p+i) << ",";
+  }
 
   return 0;
 }
